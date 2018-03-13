0cfbd911fac9ec30c9f0aa796c2629b9014222ce
Daniel Owen van Dommelen <daniel.van.dommelen@gmail.com>
2018-03-13 15:27:47 +0100

    Adding text to our curses UI

--- cloudtube.py
+++ cloudtube.py
@@ -10,6 +10,19 @@ def main(stdscr):
 
     stdscr.clear()
 
+    begin_x = 20
+    begin_y = 7
+    height  = 5
+    width   = 40
+    win     = curses.newwin(height, width, begin_y, begin_x)
+    pad     = curses.newpad(100, 100)
+
+    pad.addch(10, 10, ord('a') + (10 * 10 + 10 * 10) % 26)
+    pad.refresh(0, 0, 5, 5, 20, 75)
+
+    stdscr.addstr(0, 0, "Current Mode: STORAGE", curses.A_REVERSE)
+    stdscr.refresh()
+
     for arg in sys.argv:
         STORE = True
 
